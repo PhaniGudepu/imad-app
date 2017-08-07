60a6e193645153de9bb5bfcd84a44276a20c0d97
PhaniGudepu <glrphani@gmail.com>
2017-08-07 16:13:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ app.get('/', function (req, res) {
 app.get('/getone', function (req, res){
     res.send('first file requested');
 });
-
+app.get('/gettwo', function (req, res){
+    res.send('second file requested');
+});
+app.get('/getthree', function (req, res){
+    res.send('third file requested');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
