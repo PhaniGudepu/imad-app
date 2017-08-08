e7c5844b0fa2ecd8cc576bf08d115ce6b3e06993
PhaniGudepu <glrphani@gmail.com>
2017-08-08 23:04:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/getone', function (req, res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+     res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
 });
 app.get('/gettwo', function (req, res){
     res.send('second file requested');
