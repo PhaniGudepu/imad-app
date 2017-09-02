9bf44f3f08fc5e2e4ed014580e11fbc6c04ed4df
PhaniGudepu <glrphani@gmail.com>
2017-09-02 14:34:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,10 +3,10 @@ var get=document.getElementById('main-text');
 get.innerHTML="hi alll";
 var img = document.getElementById('madi');
 var marginAdd=0;
-function marginRight(){
+function moveRight(){
     marginAdd=marginAdd+10;
     img.style.marginLeft=marginAdd+"px";
 }
 img.onclick = function(){
-    var move=setIntervel(marginRight, 100);
+    var move=setIntervel(moveRight, 100);
 };
