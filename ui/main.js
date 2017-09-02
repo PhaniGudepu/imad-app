812a93b7b57765c5614086fd3e1b7c6b85ecd0f2
PhaniGudepu <glrphani@gmail.com>
2017-09-02 13:30:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var get=document.getElementById('main-text');
 get.innerHTML="hi alll";
+var img = document.getElementById('madi');
+img.onclick = function(){
+    img.style.marginLeft='100px';
+};
