33946231dd82f6dfcd04949f7b1ed6743c359826
PhaniGudepu <glrphani@gmail.com>
2017-09-03 22:49:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,5 @@
 var button = document.getElementById("butt");
+button.onclick = function(){
 //making the request
 var request = new XMLHttpRequest();
 // capturing the requst
@@ -11,3 +12,4 @@ request.onreadyStateChange = function(){
       }
   }
 }; 
+};
