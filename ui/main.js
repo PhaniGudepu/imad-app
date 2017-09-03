33503b58c10917d70ad896c6002f081f189fe512
PhaniGudepu <glrphani@gmail.com>
2017-09-03 23:15:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,16 @@
 var button = document.getElementById("butt");
 button.onclick = function() {
-    
-      //making the request
-var request = new XMLHttpRequest();
-// capturing the requst
-request.onreadyStateChange = function(){
-  if(request.readyState === XMLHttpRequest().DONE){
-      if(request.status === 200){
-          var counter = request.responceText;
-          var span = document.getElementById("spn");
-          span.innerHTML = counter.toString();
-      }
-  }
-};
-request.open("GET", 'http://glrphani.imad.hasura-app.io/counter');
-request.send(null);
+  //create a request object 
+  var request = new XMLHttpRequest();
+  //capturing the request
+  request.onreadystatechange = function(){
+     if(request.readyState === XMLHttpRequest.DONE);{
+         if(request.status === 200){
+             var counter = request.responseText;
+             var span = document.getElementById("spn");
+             span.innerHTML = counter.toString();
+         }
+     }
       
+  };
 };
