4f01936fa83b2738e314522bedffd7897724416e
PhaniGudepu <glrphani@gmail.com>
2017-09-03 22:27:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,7 @@
-console.log('Loaded!');
-var img = document.getElementById('madi');
-var margin = 0;
-function moveRight(){
-    margin = margin + 1;
-     img.style.marginRight=margin+'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
+var button = document.getElementById("butt");
+var counter;
+button.onclick = function() {
+  counter = counter +1;
+  var span = document.getElementById("spn");
+  span.innerHTML = counter.toString();
 };
