134a3dad1cc49a7a02fdeb3932ea6d2f457225b2
PhaniGudepu <glrphani@gmail.com>
2017-09-03 23:56:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,19 @@ button.onclick = function() {
   request.open("GET", 'http://glrphani.imad.hasura-app.io/counter');
   request.send(null);
 };
+
+//submitting names to the server
+var nameInput = document.getElementById("name");
+var name =nameInput.value;
+var submit = document.getElementById("submit-btn");
+submit.onclick = function(){
+  //name list
+  var names = ["name1","name2","name3"];
+  var list = "";
+  for(var i=0;i<names.length;i++)
+  {
+      list += "<li>" +names[i]+ "</li>";
+      }
+      var ul = document.getElementById('ull');
+      ul.innerHTML = list;
+};
